4bb573b29a5d1891b3c678b91d7d760d343c2d0f
akifa008 <akifausman2008@gmail.com>
2025-05-05 14:13:34 +0100

    Add files via upload

---
+++ data_types_practice.py
@@ -0,0 +1,37 @@
+# Integer Example
+# Number of siblings
+siblings = 4
+
+# Number of books the I have 
+books = 4
+
+print("Siblings:", siblings)
+print("Books:", books)
+
+# String Example
+# City where I live
+city = "Oldham"
+
+# Favorite subject
+favorite_subject = "Computer Science"
+
+print("City:", city)
+print("Favorite Subject:", favorite_subject)
+
+# Float Example
+# weight in kilograms
+weight = 45.0
+car_speed = 110.5
+
+print("Weight:", weight)
+print("Car Speed:", car_speed)
+
+# Boolean Example
+# Do I like coding?
+likes_coding = True
+
+# Am I hungry?
+is_hungry = False
+
+print("Likes Coding:", likes_coding)
+print("Is Hungry:", is_hungry)
