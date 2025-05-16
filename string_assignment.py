6cbcbd9073dd2b8fe2ff5b1a4b6a57cdd96cabac
akifa008 <akifausman2008@gmail.com>
2025-05-16 20:26:13 +0100

    Add files via upload

---
+++ string_assignment.py
@@ -0,0 +1,32 @@
+# Task 1 Character 
+char1 = 'A'
+char2 = '7'
+char3 = '#'
+print(char1, char2, char3)
+
+#Task 2 Strings
+animal = "Dolphin"
+sentence = "Dolphins are known for their intelligence"
+print(animal)
+print(sentence)
+
+#Task 3 concentration 
+first_name = 'Akifa'
+last_name = 'Usman'
+full_name = first_name + " " + last_name
+print(full_name)
+
+#Task 4: Repetition
+greeting = 'Hi'
+print(greeting * 5)
+
+#Task 5: Indexing
+text = "PythonRocks"
+print(text[0])  
+print(text[3])    
+print(text[-1])   
+
+#Task 6: Slicing
+sentence = "I love coding in Python"
+print(sentence[2:6])   
+print(sentence[-6:])    
