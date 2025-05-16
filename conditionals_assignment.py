1f29293767f8731b15298bf892b41b6429832586
akifa008 <akifausman2008@gmail.com>
2025-05-16 21:18:38 +0100

    Add files via upload

---
+++ conditionals_assignment.py
@@ -0,0 +1,41 @@
+# Task 1: Check Age
+age = 16  
+
+if age >= 18:
+    print("You are an adult.")  
+
+# Task 2: Age Check with Else
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")  
+
+# Task 3: Grading System
+marks = 75 
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 70:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Fail")  
+
+# Task 4: Weather Advice
+weather = "rainy"  
+
+if weather == "rainy":
+    print("Take an umbrella")
+elif weather == "sunny":
+    print("Wear sunglasses")
+elif weather == "cloudy":
+    print("It might rain later")
+
+# Task 5: Quiz – Feeling Hungry?
+hungry = True  
+
+if hungry:
+    print("Go eat something!")
+else:
+    print("Maybe have a snack later.")
