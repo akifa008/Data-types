6d42b4f1057eb4927a57c96ce5ce6fdc38134b2c
akifa008 <akifausman2008@gmail.com>
2025-05-16 21:06:08 +0100

    Add files via upload

---
+++ data_types_assignment.py
@@ -0,0 +1,28 @@
+# Task 1: Integer
+age = 16
+print("In 5 years, I will be", age + 5)
+
+# Task 2: String
+name = "Akifa"
+greeting = "Hello," + name
+print(greeting)
+
+# Task 3: Float
+height = 5.6 
+print("My height is", height, "feet.")
+
+# Task 4: Combined Use
+item = "Polaroid camera"
+quantity = 1
+price_per_item = 19.48
+total_cost = quantity * price_per_item
+print(f"I bought {quantity} {item} for a total of {round(total_cost, 2)} pounds.")
+
+# Task 5: Challenge Yourself!
+user_name = "Akifa"
+user_item = "Polaroid camera"
+user_price = 19.48
+user_quantity = 1
+
+total_price = user_price * user_quantity
+print(f"Hello {user_name}! I bought {user_quantity} {user_item} for a total of {round(total_price, 2)} pounds.")
