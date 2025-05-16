179f90d94fc785423aaf5b6e0a35d4a95ae26cbb
akifa008 <akifausman2008@gmail.com>
2025-05-16 20:34:22 +0100

    Add files via upload

---
+++ arithmetic_assignment.py
@@ -0,0 +1,31 @@
+# Task 1: Addition
+x = 7
+y = 5
+print(x + y)  
+
+# Task 2: Subtraction
+print(x - y) 
+
+# Task 3: Multiplication
+print(x * y)  
+
+# Task 4: Division
+print(x / y)  
+
+# Task 5: Floor Division
+print(x // y) 
+
+# Task 6: Modulus
+print(x % y) 
+
+# Task 7: Exponentiation
+print(x ** y) 
+
+# Task 8: All-in-One Table 
+print(f"{x} + {y} = {x + y}")
+print(f"{x} - {y} = {x - y}")
+print(f"{x} * {y} = {x * y}")
+print(f"{x} / {y} = {x / y}")
+print(f"{x} // {y} = {x // y}")
+print(f"{x} % {y} = {x % y}")
+print(f"{x} ** {y} = {x ** y}")
